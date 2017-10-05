60d4939a140fc1fe0dfbabba745896f048e1f017
Colin Di Meo <dimeo.colin@gmail.com>
2017-10-05 15:27:07 -0400

    fixed middlename typo in seed - working now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 10.times do |i|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
-  middle_name = Faker::Name.middle_name
+  middle_name = Faker::Name.name_with_middle
   username = Faker::Internet.user_name(first_name + " " + last_name, %w(. _ -))
   email = Faker::Internet.free_email(first_name + "-" + last_name)
   phone = Faker::PhoneNumber.cell_phone
